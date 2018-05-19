6ae86a43fab734b3ca495d41e5c8a57fbf7dae47
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2018-05-19 15:35:20 -0700

    C# Methods

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter number to add, subtract, and multiply by 2: ");
+            MathProblem mathProblem = new MathProblem();
+            int num = mathProblem.num1;
+            Console.WriteLine(mathProblem.add);
+            Console.WriteLine(mathProblem.sub);
+            Console.WriteLine(mathProblem.mul);
+            Console.ReadLine();
+        }
+    }
+}
