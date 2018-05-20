8935a6d31db67c27d46cd94fe99f0a246609134c
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2018-05-20 15:43:48 -0700

    Methods Exercise

---
+++ Methods/Methods/MathClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class MathClass
+    {
+        public MathClass()
+        {
+            num1 = 0;
+            num2 = 0;
+            result = 0;
+        }
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int result { get; set; }
+
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass math = new MathClass();
+            Console.WriteLine("Please enter first number: ");
+            math.num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number: ");
+            math.num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter \"Add,\" \"Subtract,\" or \"Multiply\": ");
+            string addSubMul = Console.ReadLine();
+            
+
+            if (addSubMul == "Add")
+            {
+                math = Add(math);
+                Console.WriteLine(math.num1 + " plus " + math.num2 + " equals " + math.result);
+            }
+            else if (addSubMul == "Subtract")
+            {
+                math = Subtract(math);
+                Console.WriteLine(math.num1 + " minus " + math.num2 + " equals " + math.result);
+
+            }
+            else if (addSubMul == "Multiply")
+            {
+                math = Multiply(math);
+                Console.WriteLine(math.num1 + " times " + math.num2 + " equals " + math.result);
+            }
+
+            Console.ReadLine();            
+        }
+
+        public static MathClass Add(MathClass math)
+        {
+            math.result = math.num1 + math.num2;
+            return math;
+        }
+
+        public static MathClass Subtract(MathClass math)
+        {
+            math.result = math.num1 - math.num2;
+            return math;
+        }
+
+        public static MathClass Multiply(MathClass math)
+        {
+            math.result = math.num1 * math.num2;
+            return math;
+        }
+    }
+}
