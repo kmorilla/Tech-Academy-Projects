140e615f1d1fd12aef9b7d6e6122e9079abb76d9
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2018-05-30 22:48:48 -0700

    Uploading files

---
+++ C-Sharp-Course/EnumExercise/EnumExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the current day of the week: ");
+            string userDay = Console.ReadLine();
+            bool falseDay = false;
+            
+                while (falseDay == false)
+                {
+                    foreach (DaysOfTheWeek day in Enum.GetValues(typeof(DaysOfTheWeek)))
+                    {
+                        string stringDay = Convert.ToString(day);
+                        if (userDay == stringDay)
+                        {
+                            Console.WriteLine("Correct! It is {0}.", day);
+                            falseDay = true;
+                        }
+                    }
+                    if (falseDay == false)
+                    {
+                        Console.Write("Please enter valid day: ");
+                        userDay = Console.ReadLine();
+                    }
+                }
+                Console.ReadLine();
+            
+        }
+    }
+}
---
+++ C-Sharp-Course/EnumExercise/EnumExercise/Week.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExercise
+{
+    public class Week
+    {
+        
+    }
+
+    public enum DaysOfTheWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
