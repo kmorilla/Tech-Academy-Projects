2319c20d5ff52d148001b8e8b64960c4f0d4a4db
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2018-06-02 17:46:08 -0700

    Completed exercise

---
+++ C-Sharp-Course/LogTextFile/LogTextFile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogTextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            string number = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"D:\The Tech Academy\Tech-Academy-Projects\C-Sharp-Course\LogTextFile\log.txt", true))
+            {
+                file.WriteLine(number);
+            }
+            Console.WriteLine("The number you chose is {0}.", number);
+            Console.ReadLine();
+        }
+    }
+}
