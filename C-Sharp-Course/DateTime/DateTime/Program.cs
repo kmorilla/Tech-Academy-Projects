15ac864cd3751f9e5b16eb041ca401a1d3b2b1c6
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2018-06-02 18:13:58 -0700

    Completed exercise

---
+++ C-Sharp-Course/DateTime/DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine("The date and time is " + dateTime);
+
+            Console.Write("Please enter a number of hours to add: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} + {1} hour(s) =  {2}", dateTime, number, dateTime.AddHours(number));
+            Console.ReadLine();
+        }
+    }
+}
