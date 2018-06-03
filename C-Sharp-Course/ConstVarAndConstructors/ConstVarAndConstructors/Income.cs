61ba3293fd540c9f25d060a3d1c55ca016870a53
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2018-06-03 12:03:04 -0700

    Completed exercise

---
+++ C-Sharp-Course/ConstVarAndConstructors/ConstVarAndConstructors/Income.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarAndConstructors
+{
+    public class Income
+    {
+        public Income(int grossIncome) : this(grossIncome, 0)
+        {
+        }
+        public Income(int grossIncome, int taxes)
+        {
+            NetIncome = grossIncome - taxes;
+        }
+
+        public int NetIncome { get; set; }
+    }
+}
