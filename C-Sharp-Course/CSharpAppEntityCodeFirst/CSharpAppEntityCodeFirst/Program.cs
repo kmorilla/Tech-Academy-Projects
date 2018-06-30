e8cf38b870a6c9a9ad8b858c6df3d612b6784c99
Kevyn Morilla <37521213+kmorilla@users.noreply.github.com>
2018-06-29 19:09:07 -0700

    Uploading project

---
+++ C-Sharp-Course/CSharpAppEntityCodeFirst/CSharpAppEntityCodeFirst/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAppEntityCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ReceiptContext())
+            {
+                Console.Write("Enter item purchased: ");
+                string itemPurchased = Console.ReadLine();
+                Console.Write("Enter item amount: ");
+                decimal itemAmount = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Enter date when item was purchased: ");
+                string datePurchased = Console.ReadLine();
+
+                var receipt = new Receipt { Item = itemPurchased, Amount = itemAmount, Date = datePurchased };
+                
+                db.Receipts.Add(receipt);                
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ C-Sharp-Course/CSharpAppEntityCodeFirst/CSharpAppEntityCodeFirst/Receipt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAppEntityCodeFirst
+{
+    public class Receipt
+    {
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public decimal Amount { get; set; }
+        public string Date { get; set; }
+    }
+
+    public class ReceiptContext : DbContext
+    {
+        public DbSet<Receipt> Receipts { get; set; }
+    }
+}
